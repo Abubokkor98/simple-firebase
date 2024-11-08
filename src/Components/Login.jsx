00dec3995b09c96125aef1f816e6e2642da1940e
Abubokkor98 <mail.abubokkor@gmail.com>
2024-11-08 20:12:47 +0600

    Use google sign in from firebase

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,12 +1,19 @@
-import { GoogleAuthProvider } from 'firebase/auth';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import React from 'react'
+import auth from '../Firebase/firebase.init';
 
 export default function Login() {
 
     const googleProvide = new GoogleAuthProvider();
 
     const handleGoogleSignIn = () => {
-        console.log('google is coming');
+        signInWithPopup(auth, googleProvide)
+        .then((result)=>{
+            console.log(result);
+        })
+        .catch(error=>{
+            console.log("Error khaisi mama", error);
+        })
     }
   return (  
     <div>
