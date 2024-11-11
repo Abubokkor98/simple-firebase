e12fb3e4bdbeb75e74302b657248dad966c39a40
Abubokkor98 <mail.abubokkor@gmail.com>
2024-11-11 10:55:56 +0600

    recap

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,45 +1,66 @@
-import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
-import React, { useState } from 'react'
-import auth from '../Firebase/firebase.init';
+import {
+    GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import React, { useState } from "react";
+import auth from "../Firebase/firebase.init";
 
 export default function Login() {
-    // state for display user
-    const [user, setUser] = useState(null);
+  // state for display user
+  const [user, setUser] = useState(null);
 
-    const googleProvide = new GoogleAuthProvider();
+  const googleProvide = new GoogleAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
 
-    const handleGoogleSignIn = () => {
-        signInWithPopup(auth, googleProvide)
-        .then((result)=>{
-            console.log(result.user);
-            setUser(result.user);
-        })
-        .catch(error=>{
-            console.log("Error khaisi mama", error);
-        })
-    };
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvide)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log("Error khaisi mama", error);
+      });
+  };
 
-    const handleSignOut = () =>{
-        signOut(auth)
-        .then(()=>{
-            setUser(null)
-        })
-        .catch(error=>console.log(error))
-    }
-  return (  
+  const handleGithubSignIn = () =>{
+    signInWithPopup(auth, gitHubProvider)
+    .then((result)=>{
+        console.log(result.user)
+        setUser(result.user);
+    })
+    .catch((error) => {
+        console.log("Github login Error", error);
+      });
+  }
+
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => console.log(error));
+  };
+  return (
     <div>
-        
-        
-        {
-            user ? <button onClick={handleSignOut}>SignOut</button> : <button onClick={handleGoogleSignIn}>Login with Google</button>
-        }
-        {
-            user && <div>
-                <h2>Name: {user.displayName}</h2>
-                <p>Email: {user.email}</p>
-                <img src={user.photoURL} alt="" />
-            </div>
-        }
+      {user ? (
+        <button onClick={handleSignOut}>SignOut</button>
+      ) : (
+        <>
+          <button onClick={handleGoogleSignIn}>Login with Google</button>
+          <button onClick={handleGithubSignIn}>Login with Github</button>
+        </>
+      )}
+      {user && (
+        <div>
+          <h2>Name: {user.displayName}</h2>
+          <p>Email: {user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
     </div>
-  )
+  );
 }
